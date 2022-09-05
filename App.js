e69061fc689ea89c42ced78ef36d0e8324478c57
LordeSeuZe <110058677+LordeSeuZe@users.noreply.github.com>
2022-09-05 16:56:26 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 //importr modulos
 import React from "react";
-
 //importar componentes (recursos visuais) do react-native
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
-
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 // funções principal (primeira tela)
 export default function App() {
   return (
@@ -17,7 +21,7 @@ export default function App() {
       <TextInput style={styles.textInput} placeholder="Insira seu E-mail" />
 
       <Text style={styles.labelText}>Data de nascimento</Text>
-      <TextInput style={styles.textInput} placeholder="DD/MM/AAAA"/>
+      <TextInput style={styles.textInput} placeholder="DD/MM/AAAA" />
 
       <Text style={styles.labelText}>RA</Text>
       <TextInput style={styles.textInput} placeholder="Insira seu RA" />
@@ -36,6 +40,10 @@ export default function App() {
 
       <Text style={styles.labelText}>Endereço</Text>
       <TextInput style={styles.textInput} placeholder="Insira seu Endereço" />
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.labelBtn}> SALVAR </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -49,28 +57,44 @@ const styles = StyleSheet.create({
     paddingTop: 26, //espaçamento do top
     paddingLeft: 10, // espaçamento da esquerda
     paddingBottom: 26, // espaçmento por de baixo
-    backgroundColor: '#000000', // cor de fundo
-    height: '100%', // Altura
+    backgroundColor: "#000000", // cor de fundo
+    height: "100%", // Altura
   },
   labelTitle: {
     fontSize: 36, // tamanho da fonte
-    fontWeight: '1000',
-    color: '#1eff00',
+    fontWeight: "850",
+    color: "#1eff00",
   },
   labelText: {
     paddingTop: 5,
     fontSize: 22,
-    fontWeight: '100',
-    color: '#1eff00',
+    fontWeight: "100",
+    color: "#1eff00",
   },
   textInput: {
     fontSize: 18,
-    color: '#16ba00',
-    backgroundColor: '#041a01',
-    width: '90%',
+    color: "#16ba00",
+    backgroundColor: "#041a01",
+    width: "90%",
     borderRadius: 3,
     borderWidth: 2,
-    borderColor: '#083602',
+    borderColor: "#083602",
     padding: 2,
+  },
+  btn: {
+    width: "90%",
+    height: 21,
+    marginTop: 10,
+  },
+  labelBtn: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    color: "#1eff00",
+    alignSelf: "center",
+    fontSize: 18,
+    fontWeight: "850",
+    borderRadius: 3,
+    borderWidth: 2,
+    borderColor: "#083602",
   },
 });
